ead585d8697da703db22ab39e12176f3dfb0b9d3
Peter Danenberg <pcd@roxygen.org>
2011-12-06 12:33:14 -0800

    math.combinatorics 0.0.3-SNAPSHOT doesn't exist, apparently.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cadr "1.0.0-SNAPSHOT"
+(defproject cadr "1.0.1-SNAPSHOT"
   :description "Implementing car, cdr, cadr, ..., cddddr in Clojure"
   :url "https://github.com/klutometis/cadr"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/math.combinatorics "0.0.3-SNAPSHOT"]]
+                 [org.clojure/math.combinatorics "0.0.2"]]
   :dev-dependencies [[debug "1.0.0-SNAPSHOT"]])
