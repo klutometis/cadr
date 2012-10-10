fda1aea0f1d2af6caf6f2a28dee13a74e80bb434
Peter Danenberg <pcd@roxygen.org>
2012-10-10 01:49:00 -0700

    1.0.3

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cadr "1.0.2-SNAPSHOT"
+(defproject cadr "1.0.3"
   :description "Implementing car, cdr, cadr, ..., cddddr in Clojure"
   :url "https://github.com/klutometis/cadr"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/math.combinatorics "0.0.2"]]
-  :dev-dependencies [[debug "1.0.1-SNAPSHOT"]])
+  :dev-dependencies [[debug "1.0.1"]])
